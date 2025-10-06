98cc9e39294185025e85f04c7f2bd9ae98517f65
Boubacar DIALLO <boubacar95@outlook.fr>
2025-10-06 17:38:14 +0000

    Proprite au survol d ela souris

--- citations.js
+++ citations.js
@@ -1,5 +1,11 @@
 let mot=document.getElementById("mot");
 mot.addEventListener("focus", function(){
-    mot.style.background = "linear-gradient(90deg, #0d6efd, #ffffff)"
-   
+    mot.style.background = "linear-gradient(90deg, #0d6efd, #ffffff)"  
+})
+mot.addEventListener("mouseover", function(){
+    mot.style.background = "red"  
+})
+
+mot.addEventListener("mouseout", function(){
+    mot.style.background = "goldenrod"  
 })
