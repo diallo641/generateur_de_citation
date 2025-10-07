a42bf8b7afbde254f2d2ad491088e574547f441f
Boubacar DIALLO <boubacar95@outlook.fr>
2025-10-07 09:04:07 +0000

    Variable pour recupereer les champs utilisées sur la partie HTML

--- citations.js
+++ citations.js
@@ -1,9 +1,11 @@
 let mot=document.getElementById("mot");
+let citation=document.getElementById("citation");
+let auteur=document.getElementById("auteur");
 mot.addEventListener("focus", function(){
     mot.style.background = "linear-gradient(90deg, #0d6efd, #ffffff)"  
 })
 mot.addEventListener("mouseover", function(){
-    mot.style.background = "red"  
+    mot.style.background = "linear-gradient(90deg, #87CEEB, #ffffff)"; 
 })
 
 mot.addEventListener("mouseout", function(){
